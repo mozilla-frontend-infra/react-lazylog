5d9aa2987464727142c77d07e5a2073d354a3264
Eli Perelman <eli@eliperelman.com>
2016-08-08 10:09:39 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import LogViewer from './LogViewer';
+import './app.scss';
+
+render(<LogViewer />, document.getElementById('root'));
