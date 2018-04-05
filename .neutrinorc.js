0ab0cf6e9b6994373e541a8f94ee060a8eaba0ee
Eli Perelman <eli@eliperelman.com>
2018-04-05 18:07:38 -0500

    Skip CSS extraction for now

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,10 @@
 module.exports = {
   use: [
     'neutrino-preset-mozilla-frontend-infra/styleguide',
-    'neutrino-preset-mozilla-frontend-infra/react-components',
+    ['neutrino-preset-mozilla-frontend-infra/react-components', {
+      style: {
+        extract: false,
+      }
+    }],
   ],
 };
