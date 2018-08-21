328b3cd98bc0012bf7f092be591d2bcbc28eee2f
WhoAteDaCake <doitsimplefy@gmail.com>
2018-08-21 16:24:29 +0100

    remove temporary test files

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-// Should be removed in the future
-import React from 'react';
-import { render } from 'react-dom';
-const root = document.getElementById('root');
-import NameForm from './content';
-
-render(<NameForm />, root);
