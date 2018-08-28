c4bf7309648d064105c748f8fcef0fe1717a7f99
WhoAteDaCake <doitsimplefy@gmail.com>
2018-08-28 15:37:42 +0100

    update build config

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,11 +1,5 @@
-const defaultRewrite = {
-  target: `http://localhost:3002`,
-  pathRewrite: path => path.replace(/\/$/, ''),
-};
-
-let use;
-if (process.env.NEUTRINO_ENV === 'docs') {
-  use = [
+module.exports = {
+  use: [
     'neutrino-preset-mozilla-frontend-infra/styleguide',
     [
       'neutrino-preset-mozilla-frontend-infra/react-components',
@@ -16,28 +10,7 @@ if (process.env.NEUTRINO_ENV === 'docs') {
       },
     ],
     neutrino => {
-      if (neutrino.options.command === 'styleguide:start') {
-        neutrino.config.module.rules.delete('lint');
-      }
+      neutrino.config.module.rules.delete('lint');
     },
-  ];
-} else {
-  use = [
-    [
-      '@neutrinojs/react-components',
-      {
-        devServer: {
-          hot: true,
-          proxy: {
-            '/api': defaultRewrite,
-          },
-        },
-      },
-    ],
-    '@neutrinojs/jest',
-  ];
-}
-
-module.exports = {
-  use,
+  ],
 };
