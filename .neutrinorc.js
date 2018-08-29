cf87b4d1604b816c341f2b78d0e57366d3dfe244
WhoAteDaCake <doitsimplefy@gmail.com>
2018-08-29 09:01:30 +0100

    add commnet for future build improvements

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -12,6 +12,18 @@ module.exports = {
     neutrino => {
       // Because we use prettier, we don't care about linting
       neutrino.config.module.rules.delete('lint');
+      // neutrino.config.when(neutrino.options.command === 'build', () => {
+      //   neutrino.use(fork, {
+      //     configs: {
+      //       'components-es5': {
+      //         options: {
+      //           output: 'es5',
+      //         },
+      //         use: [reactComponents, require.resolve('./react-components-es5')],
+      //       },
+      //     },
+      //   });
+      // });
     },
   ],
 };
