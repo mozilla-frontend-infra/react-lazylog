9e07f86274d9d94213024d065858360ab0881c47
WhoAteDaCake <doitsimplefy@gmail.com>
2018-08-29 08:45:35 +0100

    export search

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as DocumentViewer } from './DocumentViewer';
 export { default as DocumentMinimap } from './DocumentMinimap';
-export { search } from './DocumentSearch/format';
+export { default as Search } from './Search';
